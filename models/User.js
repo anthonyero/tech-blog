4717243f9b01fbded9b54f359c0957c6c3652c5d
anthonyero <anthonyero@gmail.com>
2024-03-27 11:20:34 -0500

    Added functionality to hash user passwords before they are stored in the database.

--- models/User.js
+++ models/User.js
@@ -1,7 +1,12 @@
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class User extends Model {}
+class User extends Model {
+	checkPassword(loginPw) {
+		return bcrypt.compareSync(loginPw, this.password)
+	}
+}
 
 User.init(
 	{
@@ -19,7 +24,7 @@ User.init(
 				len: [4,12]
 			}
 		},
-		hashed_password: {
+		password: {
 			type: DataTypes.STRING,
 			allowNull: false,
 			validate: {
@@ -28,6 +33,16 @@ User.init(
 		}	
 	},
 	{
+		hooks : {
+			beforeCreate: async (newUserData) => {
+		        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+		        return newUserData;
+			},
+	   		beforeUpdate: async (updatedUserData) => {
+		        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+		        return updatedUserData;
+			},
+		},
 		sequelize,
 		timestamps: true, 
 		freezeTableName: true,
--- seeds/seed.js
+++ seeds/seed.js
@@ -9,17 +9,15 @@ const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
   const users = await User.bulkCreate(userData, {
-    // individualHooks: true,
+    individualHooks: true,
     returning: true,
   });
 
   const posts = await Post.bulkCreate(postData, {
-    // individualHooks: true,
     returning: true,
   });
  
  // const comments = await User.bulkCreate(commentData, {
- //    individualHooks: true,
  //    returning: true,
  //  });
 
