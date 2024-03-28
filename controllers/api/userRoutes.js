8551f60d1e716c40771d02d17341398351073b55
anthonyero <anthonyero@gmail.com>
2024-03-28 00:08:37 -0500

    Defined a successful login route in userRoutes.js

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -28,37 +28,6 @@ router.post('/', async (req, res) => {
 	}
 });
 
-// Login
-
-// router.post('/login', async (req, res) => {
-// 	try {
-		
-// 		const userData = await User.findOne( {where: { username: req.body.username.toLowerCase() } });
-		
-// 		if (!userData) {
-// 			res.status(400).json({ message: 'Incorrect username, please try again' });
-//       		return;
-// 		}
-
-// 		const validPassword = await bcrypt.compare(req.body.password, userData.password);
-		
-// 		if (!validPassword) {
-//       		res.status(400).json({ message: 'Incorrect password, please try again!' });
-// 			return;
-// 		}
-
-// 		req.session.save(() => {
-// 	      req.session.user_id = userData.id;
-// 	      req.session.logged_in = true;
-	      
-// 	      res.json({ user: userData, message: 'You are now logged in!' });
-// 	    });
-
-// 	} catch (err) {
-// 		res.status(500).json(err);
-// 	}
-// });
-
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { username: req.body.username.toLowerCase() } });
@@ -70,9 +39,9 @@ router.post('/login', async (req, res) => {
       return;
     }
 
-    const passwordMatch = await userData.checkPassword(req.body.password); // Do not use bcrypt.compare
+    const validPassword = await userData.checkPassword(req.body.password); // Do not use bcrypt.compare
 
-    if (!passwordMatch) {
+    if (!validPassword) {
       res
         .status(400)
         .json({ message: 'Incorrect username or password, please try again' });
