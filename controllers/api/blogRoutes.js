72ffb5f2a9155ae3df385fe21acad1a55020c8c2
anthonyero <anthonyero@gmail.com>
2024-04-11 01:47:51 -0500

    WIP: Receiving a bad request error when trying to post a new comment. Unsure why this is the case

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -2,13 +2,23 @@ const router = require('express').Router();
 const { Post } = require('../../models');
 
 
-// New
+// New Post
 router.post('/new', async (req, res) => {
 	try {
 		req.body.user_id = req.session.user_id;
 		const postData = await Post.create(req.body);
 	    res.status(200).json(postData);
-		;
+	} catch (err) {
+		res.status(400).json(err)
+	}
+});
+
+// New comment
+router.post('/new-comment', async (req, res) => {
+	try {
+		req.body.user_id = req.session.user_id;
+		const commentData = await Comment.create(req.body);
+	    res.status(200).json(commentData);
 	} catch (err) {
 		res.status(400).json(err)
 	}
--- public/js/new-comment.js
+++ public/js/new-comment.js
@@ -1,15 +1,15 @@
 const submitComment = async (event) => {
   event.preventDefault();
 
-  // Collect values from the login form
-  const postId = document.querySelector(".new-comment-container").title;
+  // Collect values from the comment form
+  const post_id = parseInt(document.querySelector(".new-comment-container").title);
   const text = document.querySelector('#new-comment-text').value;
-  console.log(postId, text)
+
   if (text) {
     // Send a POST request to the API endpoint
     const response = await fetch('/api/blog/new-comment', {
       method: 'POST',
-      body: JSON.stringify({ postId, text }),
+      body: JSON.stringify({ post_id, text }),
       headers: { 'Content-Type': 'application/json' },
     });
 
